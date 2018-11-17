2328e1fa5bc7ba458aa0f7431832ed86a7f21407
thisiskeithb <thisiskeithb@outlook.com>
2018-11-16 17:32:41 -0800

    Initial Marlin 1.1.9 Support

--- Marlin/pins.h
+++ Marlin/pins.h
@@ -181,6 +181,8 @@
   #include "pins_GT2560_REV_A.h"      // ATmega1280, ATmega2560                     env:megaatmega1280 env:megaatmega2560
 #elif MB(GT2560_REV_A_PLUS)
   #include "pins_GT2560_REV_A_PLUS.h" // ATmega1280, ATmega2560                     env:megaatmega1280 env:megaatmega2560
+#elif MB(ONEPLUS1_wanhao)
+  #include "pins_ONEPLUS1_wanhao.h"   // ATmega2560                                 env:megaatmega2560
 
 //
 // ATmega1281, ATmega2561
